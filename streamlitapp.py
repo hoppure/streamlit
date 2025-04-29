5935f835f36d282e8a1e89b9d752643b87ca9b65
hoppure <hoppure@naver.com>
2025-04-30 02:37:27 +0900

    feat : trading log from google sheet

--- streamlitapp.py
+++ streamlitapp.py
@@ -3,17 +3,27 @@ import pandas as pd
 import numpy as np
 from streamlit_gsheets import GSheetsConnection
 
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    # initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+st.title("Stock Dashboard")
+
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(header=1, use_col = [1,2,3,4,5,6,7,])
+df = conn.read(header=1, usecols = [0,1,2,3,6,7,8,9,10,11,12,13], index_col=["Date", "종목"])
 
-with st.expander("google sheet trading log")
-    st.write(df)
+with st.expander("google sheet trading log"):
+    st.dataframe(df)
 
-
-# page = []
-
-st.title("Stock Dashboard")
-
+with st.expander("balance"):
+    st.dataframe(df)
 
